cfbdae51b319dc3c766a76b2e756a8bc8b8fa9a2
idealley <samuelpouyt@gmail.com>
2018-04-18 15:08:29 +0200

    Navigation can now handle external links and submenus at any position

--- src/components/base/SiteNavigation.vue
+++ src/components/base/SiteNavigation.vue
@@ -1,69 +1,95 @@
 <template>
     <v-navigation-drawer
-        app
-        v-model="drawer"
-        light
-        :mini-variant="mini"
-        disable-route-watcher
-        >
-        <div class="text-xs-center">
-            <i class="icon-ers icon" style="font-size:150px; color:#d0043c;"></i>
-          <!--<img src="../../assets/logo-top.png" width="200" height="112" />-->
-        </div>
+      app
+      v-model="drawer"
+      light
+      :mini-variant.sync="mini"
+      disable-route-watcher
+    >
 
-        <!-- <v-list class="pa-1">
-          <v-list-tile v-if="mini" @click.stop="mini = !mini">
+      <v-toolbar flat class="transparent" :style="!mini? 'margin-bottom:10px;' : ''">
+        <v-list class="pa-0">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <i v-if="mini" class="icon-ers icon" style="font-size:50px; color:#d0043c;"></i>
+            </v-list-tile-avatar>
+            <v-list-tile-content style="height:130px;padding: 10px 0">
+              <v-list-tile-title style="height:100px;margin-top:30px">
+              <div class="text-xs-center">
+                <i class="icon-ers icon" style="font-size:80px; color:#d0043c;"></i>
+              </div>
+              </v-list-tile-title>
+            </v-list-tile-content>
             <v-list-tile-action>
-              <v-icon>chevron_right</v-icon>
+              <v-btn icon @click.native.stop="mini = !mini">
+                <v-icon>chevron_left</v-icon>
+              </v-btn>
             </v-list-tile-action>
           </v-list-tile>
-        </v-list> -->
+        </v-list>
+      </v-toolbar>
+
+      <v-list class="pa-1">
+        <v-list-tile v-if="mini" @click.native.stop="mini = !mini">
+          <v-list-tile-action>
+            <v-icon>chevron_right</v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
 
       <v-list dense class="pt-3">
+      <span v-for="item in items" :key="item">
         <v-list-tile
           v-if="(
             !item.group && (item.private && isAuthenticated)
             || (!item.group && !item.private))"
-          v-for="item in items"
           :key="item.title"
           :to="!item.external ? item.path : null"
           :href="item.external ? item.path : null"
         >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
+          <v-list-tile-action :class="item.icon.length > 1 ? 'inline' : ''">
+            <v-icon v-for="icon in item.icon" :key="icon">{{ icon }}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            <v-list-tile-sub-title v-if="item.subtitle"> {{ item.subtitle }} </v-list-tile-sub-title>
           </v-list-tile-content>
         </v-list-tile>
 
         <v-list-group
-            v-model="item.active"
-            v-for="item in items"
-            v-if="item.group"
-            :key="item.title"
-            :prepend-icon="item.icon"
-            no-action
+          v-model="item.active"
+          v-if="item.group"
+          :key="item.title"
+          :prepend-icon="item.icon"
+          no-action
+        >
+        <v-list-tile
+          slot="activator"
+          :to="!item.external ? item.path : null"
+          :href="item.external ? item.path : null"
           >
-        <v-list-tile slot="activator" :to="item.path">
           <v-list-tile-content>
             <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            <v-list-tile-sub-title v-if="item.subtitle"> {{ item.subtitle }} </v-list-tile-sub-title>
           </v-list-tile-content>
         </v-list-tile>
 
         <v-list-tile
           v-for="subItem in item.items"
           :key="subItem.title"
-          :to="`${item.path}${subItem.path}`"
+          :to="!item.external ? `${item.path}${subItem.path}` : null"
+          :href="item.external ? item.path : null"
         >
-          <v-list-tile-action>
-            <v-icon>{{ subItem.icon }}</v-icon>
+          <v-list-tile-action :class="subItem.icon.length > 1 ? 'inline' : ''">
+            <v-icon v-for="icon in subItem.icon" :key="icon">{{ icon }}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+            <v-list-tile-sub-title v-if="subItem.subtitle"> {{ subItem.subtitle }} </v-list-tile-sub-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list-group>
+      </span>
       </v-list>
     </v-navigation-drawer>
 </template>
@@ -108,13 +134,17 @@ export default {
 </script>
 
 <style>
-  @import '../../css/fonts.styl';
+  @import '../../css/fonts.css';
 
   .logo-title {
     height: 112px !important;
   }
 
   .navigation-drawer {
-    max-height:100% !important
+    max-height:100% !important;
+  }
+
+  .inline {
+    display: block;
   }
 </style>
--- src/router/paths.js
+++ src/router/paths.js
@@ -1,37 +1,76 @@
 export default [
   {
     title: 'Home',
-    icon: 'home',
+    icon: ['s7-home icon'],
     path: '/'
   },
   {
-    title: 'External',
-    icon: 'home',
-    path: 'https://www.ersnet.org',
+    title: 'MyERS',
+    subtitle: 'create or manage your account',
+    icon: ['icon-my-ers icon'],
+    path: 'https://my.ersnet.org',
     external: true
   },
   {
     title: 'Profile',
-    icon: 'account_box',
+    icon: ['icon-business-card icon'],
     path: '/profile',
     private: true
   },
   {
-    title: 'Programme',
-    icon: 'date_range',
+    title: 'Publications',
+    icon: ['icon-publications icon'],
+    path: 'https://www.ersnet.org/publications',
+    external: true
+  },
+  {
+    title: 'ERS Vision',
+    icon: ['icon-vision icon'],
+    path: 'https://www.ersvision.org',
+    external: true
+  },
+  {
+    title: 'E-learning',
+    icon: ['icon-e-learn icon'],
+    path: 'http://www.ers-education.org/home.aspx',
+    external: true
+  },
+  {
+    title: 'Supported Websites',
     group: true,
-    path: '/programme',
+    icon: ['web'],
     items: [
       {
-        title: 'Test',
-        icon: 'label',
-        path: '/test'
+        title: 'European Lung Fundation',
+        icon: ['icon-elf icon'],
+        path: 'http://www.europeanlung.org/en/',
+        external: true
       },
       {
-        title: 'Test 2',
-        icon: 'label_outline',
-        path: '/test2'
+        title: 'Healthy Lung for life',
+        icon: ['icon-hlfl icon'],
+        path: 'http://www.europeanlung.org/en/projects-and-research/projects/healthy-lungs-for-life/home/',
+        external: true
+      },
+      {
+        title: 'TB Consilium',
+        icon: ['icon-ers icon pull-left', 'icon-who icon pull-right'],
+        path: 'http://www.europeanlung.org/en/projects-and-research/projects/healthy-lungs-for-life/home/',
+        external: true
+      },
+      {
+        title: 'FIRS',
+        subtitle: 'United For lung Health',
+        icon: [],
+        path: 'http://www.europeanlung.org/en/projects-and-research/projects/healthy-lungs-for-life/home/',
+        external: true
       }
     ]
+  },
+  {
+    title: 'Contact ERS',
+    icon: ['s7-mail icon'],
+    path: 'http://www.ers-education.org/home.aspx',
+    external: true
   }
 ]
